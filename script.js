750f8bdcce3773024a6bda3728bc9f4b3b4c72fe
Ana Ventura <anaventura1811@gmail.com>
2021-03-12 09:47:41 -0300

    Teste requisito 6

--- script.js
+++ script.js
@@ -11,11 +11,31 @@ function generateRandomColors() {
     colorBall[key].style.backgroundColor = randomColors();
   }
 }
+
+function answerInitialState() {
+  const answer = document.getElementById('answer');
+  answer.innerText = 'Escolha uma cor';
+  return answer;
+}
+
 const resetGame = document.querySelector('#reset-game');
+resetGame.addEventListener('click', answerInitialState);
 resetGame.addEventListener('click', generateRandomColors);
 
 window.onload = function initialize() {
   generateRandomColors();
 };
 
+/* function saveMyScore() {
+  localStorage.myListOfTasks = myListOfTasks.innerHTML;
+}
+const btnSaveMyTasks = document.getElementById('salvar-tarefas');
+
+if (typeof Storage !== 'undefined' && localStorage.myListOfTasks) {
+  myListOfTasks.innerHTML = localStorage.myListOfTasks;
+}
+btnSaveMyTasks.addEventListener('click', saveMyTasks); */
+
 // score usar armazenamento no localStorage;
+// usar incremento de 3 pontos para cada acerto;
+// uma regra para associar o valor que aparece, com o clicado.
