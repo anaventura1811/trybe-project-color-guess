1d17969c2fb7e0e478d2f78d355b21080b021667
Ana Ventura <anaventura1811@gmail.com>
2021-03-12 11:04:00 -0300

    Tentativa requisito 2

--- script.js
+++ script.js
@@ -2,36 +2,30 @@ function randomColors() {
   const r = Math.ceil(Math.random() * 255);
   const g = Math.ceil(Math.random() * 255);
   const b = Math.ceil(Math.random() * 255);
-  return `rgb(${r}, ${g}, ${b})`;
+  return `(${r}, ${g}, ${b})`;
 }
 
+const colorBall = document.getElementsByClassName('ball');
 function generateRandomColors() {
-  const colorBall = document.getElementsByClassName('ball');
   for (let key = 0; key < colorBall.length; key += 1) {
-    colorBall[key].style.backgroundColor = randomColors();
+    colorBall[key].style.backgroundColor = `rgb${randomColors()}`;
   }
-}
-
-function answerInitialState() {
-  const answer = document.getElementById('answer');
-  answer.innerText = 'Escolha uma cor';
-  return answer;
 }
 
 // Para cumprir requisito 2
 
-function randomColor() {
-  const r = Math.ceil(Math.random() * 255);
-  const g = Math.ceil(Math.random() * 255);
-  const b = Math.ceil(Math.random() * 255);
-  return `(${r}, ${g}, ${b})`;
-}
-
 function rgbColorGuess() {
   const rgbColor = document.getElementById('rgb-color');
-  rgbColor.innerText = `${randomColor()}`;
+  rgbColor.textContent = `${randomColors()}`;
   return rgbColor;
 }
+
+const answer = document.getElementById('answer');
+function answerInitialState() {
+  answer.innerText = 'Escolha uma cor';
+  return answer;
+}
+
 const resetGame = document.querySelector('#reset-game');
 resetGame.addEventListener('click', answerInitialState);
 resetGame.addEventListener('click', generateRandomColors);
@@ -41,6 +35,114 @@ window.onload = function initialize() {
   generateRandomColors();
 };
 
+const colorOne = document.getElementsByClassName('ball')[0];
+const colorTwo = document.getElementsByClassName('ball')[1];
+const colorThree = document.getElementsByClassName('ball')[2];
+const colorFour = document.getElementsByClassName('ball')[3];
+const colorFive = document.getElementsByClassName('ball')[4];
+const colorSix = document.getElementsByClassName('ball')[5];
+
+function colorOneSelected() {
+  if (colorOne.classList.contains('selected')) {
+    colorTwo.classList.remove('selected');
+    colorThree.classList.remove('selected');
+    colorFour.classList.remove('selected');
+    colorFive.classList.remove('selected');
+    colorSix.classList.remove('selected');
+  }
+}
+function colorTwoSelected() {
+  if (colorTwo.classList.contains('selected')) {
+    colorOne.classList.remove('selected');
+    colorThree.classList.remove('selected');
+    colorFour.classList.remove('selected');
+    colorFive.classList.remove('selected');
+    colorSix.classList.remove('selected');
+  }
+}
+
+function colorThreeSelected() {
+  if (colorThree.classList.contains('selected')) {
+    colorOne.classList.remove('selected');
+    colorTwo.classList.remove('selected');
+    colorFour.classList.remove('selected');
+    colorFive.classList.remove('selected');
+    colorSix.classList.remove('selected');
+  }
+}
+
+function colorFourSelected() {
+  if (colorFour.classList.contains('selected')) {
+    colorOne.classList.remove('selected');
+    colorTwo.classList.remove('selected');
+    colorThree.classList.remove('selected');
+    colorFive.classList.remove('selected');
+    colorSix.classList.remove('selected');
+  }
+}
+function colorFiveSelected() {
+  if (colorFive.classList.contains('selected')) {
+    colorOne.classList.remove('selected');
+    colorTwo.classList.remove('selected');
+    colorThree.classList.remove('selected');
+    colorFour.classList.remove('selected');
+    colorSix.classList.remove('selected');
+  }
+}
+function colorSixSelected() {
+  if (colorSix.classList.contains('selected')) {
+    colorOne.classList.remove('selected');
+    colorTwo.classList.remove('selected');
+    colorThree.classList.remove('selected');
+    colorFour.classList.remove('selected');
+    colorFive.classList.remove('selected');
+  }
+}
+
+function colorOneSelection() {
+  colorOne.classList.add('selected');
+  colorOneSelected();
+}
+colorOne.addEventListener('click', colorOneSelection);
+
+function colorTwoSelection() {
+  colorTwo.classList.add('selected');
+  colorTwoSelected();
+}
+colorTwo.addEventListener('click', colorTwoSelection);
+
+function colorThreeSelection() {
+  colorThree.classList.add('selected');
+  colorThreeSelected();
+}
+colorThree.addEventListener('click', colorThreeSelection);
+
+function colorFourSelection() {
+  colorFour.classList.add('selected');
+  colorFourSelected();
+}
+colorFour.addEventListener('click', colorFourSelection);
+
+function colorFiveSelection() {
+  colorFive.classList.add('selected');
+  colorFiveSelected();
+}
+colorFive.addEventListener('click', colorFiveSelection);
+
+function colorSixSelection() {
+  colorSix.classList.add('selected');
+  colorSixSelected();
+}
+colorSix.addEventListener('click', colorSixSelection);
+
+/* function correctIncorrectGuess() {
+  if () {
+    answer.innerText = 'Acertou!';
+  } else {
+    answer.innerText = 'Errou! Tente novamente!';
+  }
+}
+*/
 /* function saveMyScore() {
   localStorage.myListOfTasks = myListOfTasks.innerHTML;
 }
